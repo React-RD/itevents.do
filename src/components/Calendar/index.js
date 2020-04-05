dbde68f23a48d6e15877bb5b62e6f96c9c581cf0
Misael Taveras <macrot29@gmail.com>
2020-04-05 19:41:05 -0400

    fix: start showing events from the current date
    The previous implementation had an undesired behaviour, it showed events from the first month with events no matter what the current month was.
    
    In this new implementation, we check if the current month is in the list and use it as the starting point. If not, we use the first month with events.
    
    In the future we'll want a more sophisticated way of doing this, for example, rendering the monthly calendar regardless of the events list for that month.

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -2,20 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { useConfig } from '../ConfigContext'
+import { toEventMonthFormat } from '../../utils/date'
+
 import { Month } from './Month'
 
 export const Calendar = ({ showModal, events }) => {
   const { limitMonthInTheFuture = 1 } = useConfig()
-  return Object.keys(events)
-    .slice(0, limitMonthInTheFuture)
-    .map(month => (
-      <Month
-        events={events[month]}
-        showModal={showModal}
-        date={month}
-        key={month}
-      />
-    ))
+  const MONTHS = Object.keys(events)
+  const currentMonth = toEventMonthFormat(new Date())
+  const currentMonthIndex =
+    MONTHS.indexOf(currentMonth) >= 0 ? MONTHS.indexOf(currentMonth) : 0
+  return MONTHS.slice(currentMonthIndex, limitMonthInTheFuture).map(month => (
+    <Month
+      events={events[month]}
+      showModal={showModal}
+      date={month}
+      key={month}
+    />
+  ))
 }
 
 Calendar.propTypes = {
